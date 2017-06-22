ef9e4f2584edbdf9b4eb5b1b665955775f49504f
Shajee <Shajee>
2017-06-22 15:36:01 -0400

    Added env port

--- server.js
+++ server.js
@@ -81,5 +81,6 @@ app.post('/comment',function(req,res){
 });
 
 
-app.listen(3000);
-console.log('Server is listening on port 3000');
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Listening on port' + process.env.PORT);
+});
